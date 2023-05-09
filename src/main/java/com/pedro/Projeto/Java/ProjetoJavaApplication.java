5c1ea15580c0b98fa972f4c375e87472f56c0acc
Pedro Lucas <pedroluc1232@gmail.com>
2023-05-08 21:30:21 -0300

    project created

---
+++ src/main/java/com/pedro/Projeto/Java/ProjetoJavaApplication.java
@@ -0,0 +1,13 @@
+package com.pedro.Projeto.Java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedro/Projeto/Java/ProjetoJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedro.Projeto.Java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
